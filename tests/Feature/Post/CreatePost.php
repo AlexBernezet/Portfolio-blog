6d8c42721f6a82bd7ff7a514d899039572f0f4f8
alexandrebernezet <alexandre.bernezet@gmail.com>
2021-04-09 13:04:33 +0200

    Created unit test

---
+++ tests/Feature/Post/CreatePost.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Feature\Post;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class CreatePost extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function user_can_create_a_post_if_correct_data_is_provided(): void
+    {
+
+    }
+}
