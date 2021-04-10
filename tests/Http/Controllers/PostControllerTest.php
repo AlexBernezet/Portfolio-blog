bbe5eba289e0b8006abae8d4baa00ea677b3bc24
alexandrebernezet <alexandre.bernezet@gmail.com>
2021-04-10 17:02:38 +0200

    Test we can update a Post with PostRepository, and we can update a post and we are redirect to the Post on PostController@update

--- tests/Http/Controllers/PostControllerTest.php
+++ tests/Http/Controllers/PostControllerTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Http\Controllers;
 
 use App\Http\Controllers\PostController;
+use App\Models\Post;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -13,19 +14,32 @@ use Tests\TestCase;
 class PostControllerTest extends TestCase
 {
     use RefreshDatabase;
+    private User $user;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->user = User::factory()->create();
+    }
 
     public function testStore(): void
     {
-        $user = User::factory()->create();
         $attributes = [
             'title' => 'Title',
             'content' => "lorem ipsum ipsum lorem",
             'slug' => 'slug-of-article'
         ];
 
-        $response = $this->actingAs($user)->post('/posts', $attributes);
-
+        $response = $this->actingAs($this->user)->post('/posts', $attributes);
         $response->assertRedirect('/admin/posts');
+    }
 
+    public function testUpdate(): void {
+        $post = Post::factory()->create();
+        $response = $this->actingAs($this->user)->put("/posts/{$post->id}", array(
+            "title" => "this is a new title",
+            "content" => "new content",
+        ));
+        $response->assertRedirect("/blog/{$post->slug}");
     }
 }
--- tests/Repositories/PostRepositoryTest.php
+++ tests/Repositories/PostRepositoryTest.php
@@ -4,24 +4,41 @@ namespace Tests\Repositories;
 
 use App\Models\Post;
 use App\Models\User;
+use App\Repositories\Interfaces\PostRepositoryInterface;
 use App\Repositories\PostRepository;
-use PHPUnit\Framework\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
 
 class PostRepositoryTest extends TestCase
 {
+    use RefreshDatabase;
+    protected PostRepositoryInterface $postRepository;
 
-    public function testCreate()
+    protected function setUp(): void
     {
-        $postRepository = new PostRepository();
+        $this->postRepository = new PostRepository();
+        parent::setUp();
+    }
+
+    public function testCreate(): void
+    {
+
         $attributes = [
             'title' => 'Title',
             'content' => "lorem ipsum ipsum lorem",
             'slug' => 'slug-of-article'
         ];
-        $post = $postRepository->create($attributes);
+        $post = $this->postRepository->create($attributes);
         self::assertInstanceOf(Post::class, $post);
         self::assertEquals(1, $post->read_time);
         self::assertEquals($attributes['title'], $post->title);
         self::assertEquals($attributes['content'], $post->content);
+    }
+
+    public function testUpdate(): void {
+        $post = Post::factory()->create();
+        $updateData = ['title' => "new title"];
+        $post = $this->postRepository->update($updateData, $post->id);
+        self::assertEquals($updateData['title'], $post->title);
     }
 }
