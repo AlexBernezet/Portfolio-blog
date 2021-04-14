b1625de1b269c99e950ad4b2534aad0eb0d3811a
alexandrebernezet <alexandre.bernezet@gmail.com>
2021-04-14 20:20:49 +0200

    Created SkillControllerTest

---
+++ tests/Http/Controllers/SkillControllerTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Http\Controllers;
+
+use App\Http\Controllers\SkillController;
+use PHPUnit\Framework\TestCase;
+
+class SkillControllerTest extends TestCase
+{
+
+    public function testIndex()
+    {
+
+    }
+
+    public function testDestroy()
+    {
+
+    }
+
+    public function testUpdate()
+    {
+
+    }
+
+    public function testStore()
+    {
+
+    }
+}
