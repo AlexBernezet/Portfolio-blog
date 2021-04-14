35693748df1993d40b421986b16f77a34d76b066
alexandrebernezet <alexandre.bernezet@gmail.com>
2021-04-14 13:28:33 +0200

    Created SkillRepository @ create method and testing

--- app/Repositories/SkillRepository.php
+++ app/Repositories/SkillRepository.php
@@ -4,7 +4,14 @@
 namespace App\Repositories;
 
 
+use App\Models\Skill;
+use JetBrains\PhpStorm\Pure;
+
 class SkillRepository
 {
-
+    public function create($data): Skill {
+        $skill = new Skill($data);
+        $skill->save();
+        return $skill;
+    }
 }
--- tests/Repositories/SkillRepositoryTest.php
+++ tests/Repositories/SkillRepositoryTest.php
@@ -2,10 +2,43 @@
 
 namespace Tests\Repositories;
 
+use App\Models\Skill;
+use App\Repositories\Interfaces\SkillRepositoryInterface;
 use App\Repositories\SkillRepository;
-use PHPUnit\Framework\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\DB;
+use Tests\TestCase;
 
 class SkillRepositoryTest extends TestCase
 {
+    use RefreshDatabase;
+    use DatabaseMigrations;
 
+    /**
+     * @var SkillRepository
+     */
+    private SkillRepository $skillRepository;
+
+    protected function setUp(): void
+    {
+        $this->skillRepository = new SkillRepository();
+        parent::setUp();
+    }
+
+    public function testCreate(): void
+    {
+        $attributes = [
+            'name' => 'PHP',
+            'position' => '1',
+            'is_bold' => true,
+            'type' => 'backend'
+        ];
+
+        $skill = $this->skillRepository->create($attributes);
+        self::assertEquals($attributes['name'], $skill->name);
+        self::assertEquals($attributes['type'], $skill->type);
+        $this->assertDatabaseCount('skills', 1);
+
+    }
 }
